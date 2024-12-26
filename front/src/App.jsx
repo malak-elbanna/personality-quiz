bb0711439fa7fb77c17fa19a2c43d51c513c02f9
mernaahmed2020 <mememero5050@gmail.com>
2024-12-26 16:41:25 +0200

    added nav bar,footer, home page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/tailwind.css';
 import QuizCollection from './pages/QuizCollection';
+import  Home from './pages/Home';
+
 
 const App = () => {
   return (
     <Router>
+    
+   
       <Routes>
-        <Route path='/collection' element={<QuizCollection />} />
+      <Route path="/" element={<Home/>} />
+      
+        
       </Routes>  
+    
+    
     </Router>
   );
 }
