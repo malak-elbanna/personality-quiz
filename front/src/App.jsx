4b9dbdffa52460e4619617a853a1234bc4dc4f19
mernaahmed2020 <mememero5050@gmail.com>
2024-12-26 19:27:34 +0200

    latest

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/tailwind.css';
 import QuizCollection from './pages/QuizCollection';
-
+import Gallery from './pages/Gallery';
 import Home from './pages/Home';
 import Quiz1 from './pages/Quiz1';
 import Quiz2 from './pages/Quiz2';
@@ -13,6 +13,8 @@ const App = () => {
       <Routes>
         {/* Home Page */}
         <Route path="/" element={<Home />} />
+
+        <Route path="/gallery" element={<Gallery />}/>
 
         {/* Quiz Collection */}
         <Route path="/collection" element={<QuizCollection />} />
