3b63c2c89bfcb17938f79beb9d45439dbfb534be
Basmalamoustafa <75669135+Basmalamoustafa@users.noreply.github.com>
2024-12-27 14:06:57 +0200

    Update Navbar.jsx

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -24,6 +24,7 @@ const Navbar = () => {
             <Link to="/about" className="text-purple-600 hover:text-purple-800 hover:underline hover:underline-offset-8">About Us</Link>
             <Link to="/blog" className="text-purple-600 hover:text-purple-800 hover:underline hover:underline-offset-8">Blog</Link>
             <Link to="/gallery" className="text-purple-600 hover:text-purple-800 hover:underline hover:underline-offset-8">Gallery</Link>
+            <Link to="/FAQs" className="text-purple-600 hover:text-purple-800 hover:underline hover:underline-offset-8">FAQs</Link>
           </div>
         </div>
         
