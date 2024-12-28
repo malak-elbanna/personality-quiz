37154bbcec2b101ba65fe7acd0ee2fbc910da7c3
Mustafa Muhhamed <s-mustafa.muhhamed@zewailcity.edu.eg>
2024-12-28 16:04:30 +0200

    Route 404 page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/tailwind.css';
 import QuizCollection from './pages/QuizCollection';
+import Gallery from './pages/Gallery';
 import Home from './pages/Home';
 import Quiz1 from './pages/Quiz1';
 import Quiz2 from './pages/Quiz2';
 import Quiz3 from './pages/Quiz3';
-import Gallery from './pages/Gallery';
 import Success from './pages/Success';
-import Navbar from './components/Navbar';
 import SignUp from './pages/SignUp';
-import Footer from './components/Footer';
-import AboutUs from './pages/AboutUs';
-import FAQs from './pages/FAQs';
 import Blog from './pages/Blog';
 import PersonalityType from './pages/PersonalityType';
-import QuizHistory from './pages/QuizHistory';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -23,26 +21,19 @@ const App = () => {
       <Routes>
         {/* Home Page */}
         <Route path="/" element={<Home />} />
-        
-
-        <Route path="/gallery" element={<Gallery />}/>
+        <Route path="/gallery" element={<Gallery />} />
 
         {/* Quiz Collection */}
         <Route path="/collection" element={<QuizCollection />} />
-        <Route path="/about" element={<AboutUs />} />
         
         {/* Quizzes */}
         <Route path="/quiz/1" element={<Quiz1 />} />
         <Route path="/quiz/2" element={<Quiz2 />} />
         <Route path="/quiz/3" element={<Quiz3 />} />
-        <Route path="/quiz/:id" element={<Quiz1 />} />
         <Route path="/success" element={<Success />} />
 
         {/* Sign Up */}
         <Route path="/signup" element={<SignUp />} />
-
-        {/* FAQs */}
-        <Route path="/FAQs" element={<FAQs />} />
 
         {/* Blog */}
         <Route path="/blog" element={<Blog />} />
@@ -50,8 +41,8 @@ const App = () => {
         {/* Personality Type */}
         <Route path="/personality-type" element={<PersonalityType />} />
 
-        <Route path="/quiz-history" element={<QuizHistory />} />
-
+        {/* 404 Page */}
+        <Route path="*" element={<NotFound />} /> 
       </Routes>
       <Footer />
     </Router>
